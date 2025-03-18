7132cf71c720151acc17e2c6574ad90adc1ff183
imseigen <seigenjane@gmail.com>
2025-03-18 07:32:12 +0700

    “menambahkan class PemrosesData dengan method DapatkanNilaiTerbesar”

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class PemrosesData
+{
+    public T DapatkanNilaiTerbesar<T>(T a, T b, T c)
+    {
+        dynamic Max = a;
+        if (b > Max) Max = b;
+        if (c > Max) Max = c;
+        return Max;
+
+    }
+    
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PemrosesData data = new PemrosesData();
+        int nilai = 10;
+        int nilai2 = 20;
+        int nilai3 = 30;
+        data.DapatkanNilaiTerbesar(nilai, nilai2, nilai3);
+
+        Console.WriteLine($"nilai Terbesar: {data.DapatkanNilaiTerbesar(nilai, nilai2, nilai3)}");
+        
+    }
+}
